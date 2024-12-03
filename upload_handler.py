2d6b0a3390362d8f0396da99992b8d119ce56d52
Norman Laptop Linux <norman.rudo@gmail.com>
2024-12-03 15:00:41 +0100

    Now allowing multiple uploads at once and adding the .gif format as a acceptable media format

--- upload_handler.py
+++ upload_handler.py
@@ -1,20 +1,26 @@
-from flask import Flask, request, redirect, url_for
 import os
 
+from flask import request, redirect
+
+
 def upload_image(app):
+    @app.route('/upload_image', methods=['POST'])
+    def handle_upload():
+        if 'image_file' not in request.files:  # Check for 'images' instead of 'image_file'
+            return redirect(request.url)  # Redirect back if no files selected
 
-  @app.route('/upload_image', methods=['POST'])
-  def handle_upload():
-      if 'image_file' not in request.files:
-          return redirect(request.url)  # Redirect back to upload page if no file selected
-      
-      image_file = request.files['image_file']
-      filename = image_file.filename  # Sanitize filename using werkzeug.secure_filename
-      image_path = os.path.join(app.config['STATIC_FOLDER'], filename)
+        images = request.files.getlist('image_file')  # Get a list of uploaded files
 
-      try:
-          image_file.save(image_path)
-          return 'Image uploaded successfully!'
-      except Exception as e:
-          return f"Error saving image: {e}"
+        # Loop through each uploaded image
+        for image in images:
+            if image.filename == '':
+                return 'No selected file'
+            filename = image.filename  # Sanitize filename
+            image_path = os.path.join(app.config['STATIC_FOLDER'], filename)
 
+            try:
+                image.save(image_path)
+            except Exception as e:
+                return f"Error saving image {filename}: {e}"
+
+        return f'{len(images)} images uploaded successfully!'  # Return success message with number of images
