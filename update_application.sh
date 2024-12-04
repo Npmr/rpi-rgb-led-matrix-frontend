45eee8567acf50d92768c3a3aff5fca86bc3c105
Norman Laptop Linux <norman.rudo@gmail.com>
2024-12-04 18:42:01 +0100

    Added a new sh script to create a update logic in the software and created a first chaptar in the README.md file for it

---
+++ update_application.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update  # To get the latest package lists
+#apt-get install <package name> -y
+
+1. kill the python process
+2. git pull or git up from the last release
+3. start python process again
+
+
+wget https://github.com/Npmr/rpi-rgb-led-matrix-frontend.git
